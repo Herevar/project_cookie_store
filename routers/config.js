5ccbfb42edc7da37042e83c45d23c2a9c9753125
herevar <laghbert@gmail.com>
2023-03-10 11:08:15 +0000

    Update 4 files

--- routers/config.js
+++ routers/config.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { cookie_add } = require('../data/cookie_data');
 const { getAddFromReq } = require('../utils/get_add_from_req');
 const configuratorRouter = express.Router();
 
@@ -15,8 +16,24 @@ configuratorRouter
     .get('/aditions/:add', (req,res)=>{
         const {add} = req.params;
         
+        //dodajemy walidacje czy taki dodatek wogole istnieje
+        if ( !cookie_add[add]){
+            //return res.send('nie ma takiego bicia')    
+            return res.render('error', {
+                descripiton : `do chaupy krowy doić - nie ma takiego adda jak ${add}`
+            })
+        }   
+        
+
+             
         const addons = getAddFromReq(req)
         //jezeli są jakies ciastka z dodatkami to sparsuj jak nie to stworz liste
+        
+        if ( addons.includes(add)){    
+            return res.render('error', {
+                descripiton : `już jest dodany taki składnik; ${add}`
+            })
+        }   
         addons.push(add)
         
         res
@@ -24,7 +41,7 @@ configuratorRouter
         // res.send(lightordark)
             .render('config_file/added', {add})
 })
-
+// miejscu gdzie tworzone były pozycje dodane do listy dodajemy link do usuwania z listy i tutaj opcje usuwania
 .get('/del/:add', (req,res)=>{
         const {add} = req.params;
         
